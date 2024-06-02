117cd26725819414b9c758374e11b4e06f3a97f8
Higorhrs <higorrosa677@gmail.com>
2024-06-02 01:39:55 -0300

    adicicao_custo_adicional2

--- Sistema.c
+++ Sistema.c
@@ -20,21 +20,27 @@ main(){
 	printf("\nDeseja adicionar outro custo?\nDigite (1) para adicionar ou (2) para prosseguir:\n");
 	scanf("%d", &adicionar_custo_ou_nao);
 	adicionar_custo_ou_nao;
+	
+	/*Somando o taltal de custos sem adicional*/
 	total_gastos_fixos = agua + energia + telefone + internet + aluguel;
+	
+	/*Criando um loop, enquanto o usuário digitar 1 ele pode adicionar um novo item*/
 	while(adicionar_custo_ou_nao==1){
 	if(adicionar_custo_ou_nao == 1){
 	printf("Qual o nome do custo adicional?:\n");
 	scanf("%s", &adicional);
 	
-	printf("Qual o custo %s?: \n", adicional);scanf("%f", &preco_adicional);
+	printf("Qual o custo %s?: \n", adicional)/*Aqui no (%s) toda vez que o usuário digitar o nome do novo custo a variável assumirá o novo nome*/;
+	scanf("%f", &preco_adicional);
+	
+	/*Somando custos adicionais*/
 	total_gastos_fixos += preco_adicional;
 	printf("\nDeseja adicionar outro custo?\nDigite (1) para adicionar ou (2) para prosseguir:\n");
 	scanf("%d", &adicionar_custo_ou_nao);
 	i++;
 	}
 	}		
-	/* Calculando o total dos gastor fixos*/
-	
+	/* Imprinmento para o usuário o valor total*/
 	printf("\n\nTotal de gastos fixos R$:%.2f", total_gastos_fixos);
 
 
